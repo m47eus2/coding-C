c61becc6c68e0b27f546217002754b605d7c70dd
m47eus2 <158043802+m47eus2@users.noreply.github.com>
2026-02-16 16:55:08 +0100

    Adding k&r

---
+++ k&r/1_1_helloWorld.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+int main(void){
+    printf("Hello, world\n");
+    return 0;
+}
+
+// Sekwencje sterujące (escape sequence)
+/*
+    \n  -> znak nowej lini
+    \t  -> znak tabulatora
+    \b  -> cofnięcie pozycji o jeden znak
+    \"  -> "
+    \\  -> \
+*/
---
+++ k&r/1_2_zmienne.c
@@ -0,0 +1,55 @@
+#include <stdio.h>
+
+/*Tabela przeliczeniowa stopni Fahrenheita na Celsjusza*/
+
+int main(void){
+    // Deklaracja zmiennych
+    int fahr, cels;
+    int lower, upper, step;
+
+    // Operacje na liczbach stałoprzecinkowych
+
+    // Instrukcje przypisania
+    lower = 0;
+    upper = 300;
+    step = 20;
+    fahr = lower;
+
+    while(fahr <= upper){
+        cels = 5 * (fahr-32) / 9; // Obcięcie części ułamkowej wyniku
+        printf("%d\t%d\n",fahr,cels);
+        fahr = fahr + 20;
+    }
+
+    // Arytmetyka zmiennoprzecinkowa
+    float ffahr, fcels;
+
+    lower = 0;
+    upper = 300;
+    step = 20;
+    ffahr = lower;
+
+    while(ffahr <= upper){
+        fcels = (5.0/9.0) * (ffahr-32.0);
+        printf("%3.0f %6.1f\n",ffahr,fcels);
+        ffahr = ffahr + step; // Gdy operator ma chociaż jeden operand zmiennoprzecinkowy liczba int zostaje przekształcona na float
+    }
+
+    return 0;
+}
+
+// Modyfikatory w printf
+/*
+    %d      -> liczba całkowita
+    %6d     -> liczba całowita na conajmniej 6 miejscach (wyrównanie do prawej)
+    %f      -> liczba zmiennoprzecinkowa
+    %6f     -> liczba zmiennoprzecinkowa na conajmniej 6 miejscach (wyrównanie do prawej)
+    %.2f    -> liczba zmiennoprzecinkowa 2 znaki po przecinku
+    %6.2f   -> liczba zmiennoprzecinkowa na conajmniej 6 miejscach, 2 znaki po przecinku 
+
+    %o      -> liczba w notacji ósemkowej
+    %x      -> liczba w notacji szesnastkowej
+    %c      -> znak
+    %s      -> ciąg znaków 
+    %%      -> %
+*/
