35c21ed086c659a31ee86f6a6416571f3023c116
m47eus2 <158043802+m47eus2@users.noreply.github.com>
2026-02-18 13:34:20 +0100

    Update k&r

---
+++ k&r/1_5_4_zliczanieSlow.c
@@ -0,0 +1,31 @@
+#include <stdio.h>
+
+// Zliczanie słów, wierszy i znaków
+
+// Stałe symboliczne flag
+#define IN 1
+#define OUT 0
+
+int main(void){
+    int c, nl, nw, nc, state;
+
+    // Przypiasanie jest wyrażeniem posiadającym wartość (lewej strony po wykonaniu przypisania)
+    // Kolejność przypisań - od prawej do lewej
+    nl = nw = nc = 0;
+
+    state = OUT;
+    while((c = getchar()) != EOF){
+        nc++;
+        if(c == '\n')
+            nl++;
+        if(c == ' ' || c == '\n' || c == '\t') // 
+            state = OUT;
+        else if(state == OUT){
+            state = IN;
+            nw++;
+        }
+    }
+    printf("Words: %d, Lines: %d, Chars: %d\n", nw, nl, nc);
+
+    return 0;
+}
